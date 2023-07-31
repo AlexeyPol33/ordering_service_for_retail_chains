5336a138bf8930b90c5f296a8fac6f19aa7c8a31
Alexey Poltavskiy <79776177342@yandex.ru>
2023-07-31 21:30:56 +0300

    add models and tests

--- app/models.py
+++ app/models.py
@@ -1,3 +1,177 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class UserTypeChoices(models.TextChoices):
+    SHOP = 'Магазин'
+    BUYER = 'Покупатель'    
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff',False)
+        extra_fields.setdefault('is_superuser',False)
+        return self._create_user(email,password,**extra_fields)
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser',True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self._create_user(email, password, **extra_fields)
+    
+class User(AbstractUser):
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(_('email addres'), unique=True)
+    company = models.CharField(verbose_name='Компания', max_length=40, blank=True)
+    position = models.CharField(verbose_name='Должность', max_length=40, blank=True)
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        _('username'),
+        max_length=200,
+        help_text=_('Required. 200 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _('A user with that username already exists.'),
+        },
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=False,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts. '
+        ),
+    )
+    type = models.CharField(verbose_name='Тип пользователя',
+                             choices=UserTypeChoices.choices,
+                             max_length=10,
+                             default=UserTypeChoices.BUYER)
+    
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Список пользователей'
+        ordering = ('email',)
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название',unique=True)
+    url = models.URLField(verbose_name='Ссылка', null=True, blank=True,unique=True)
+    filename = models.CharField(max_length=100, verbose_name='Название файла',blank=True,null=True)
+
+    class Meta:
+        verbose_name = 'Магазин'
+        verbose_name_plural = "Список магазинов"
+        ordering = ('-name',)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название', unique=True)
+    shops = models.ManyToManyField(Shop, through='Shops_Categories',blank=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Список Категорий'
+        ordering = ('-name',)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Shops_Categories(models.Model):
+    categorys = models.ForeignKey(Category, on_delete=models.CASCADE)
+    shops = models.ForeignKey(Shop,on_delete=models.CASCADE)
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,verbose_name='Категория', null=True, blank=True,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100,verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Список Продуктов'
+        ordering = ('-name',)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class ProductInfo(models.Model):
+    product = models.ForeignKey(Product,verbose_name='Продукт',on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop,verbose_name='Магазин', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100,verbose_name='Название')
+    quantity = models.FloatField(verbose_name='Количество',validators=[MinValueValidator(0)])
+    price = models.FloatField(verbose_name='Цена',validators=[MinValueValidator(0)])
+    price_rrc = models.CharField(max_length=3,verbose_name='Валюта')
+
+class Parameter(models.Model):
+    name = models.CharField(max_length=100,verbose_name='Название',unique = True)
+
+    class Meta:
+        verbose_name = 'Параметр'
+        verbose_name_plural = 'Список Параметров'
+        ordering = ('-name',)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class ProductParameter(models.Model):
+    Product_info = models.ForeignKey(ProductInfo,on_delete=models.CASCADE,verbose_name='Информация о продукте')
+    parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE, verbose_name='Параметер')
+    value = models.FloatField(verbose_name='Значение')
+
+
+class Order(models.Model):
+    class OrderStatusChoice(models.TextChoices):
+        BASKET = 'Статус корзины'
+        NEW = 'Новый'
+        CONFIRMED = 'Подтвержден'
+        ASSEMBLED = 'Собран'
+        SENT = 'Отправлен'
+        DELIVERED = 'Доставлен'
+        CANCELED = 'Отменен'
+    
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Список заказов'
+
+    user = models.ForeignKey(User, verbose_name='Пользователь',on_delete=models.CASCADE)
+    dt = models.DateTimeField(auto_now_add = True, verbose_name='Дата создания')
+    status = models.CharField(choices=OrderStatusChoice.choices,
+                                default=OrderStatusChoice.NEW,
+                                verbose_name='Статус')
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,verbose_name='Заказ',on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,verbose_name='Продукт',on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop,verbose_name='Магазин',on_delete=models.CASCADE)
+    quantity = models.FloatField(validators=[MinValueValidator(0)], verbose_name='Количество')
+
+class Contact(models.Model):
+    type = models.CharField(max_length=100, verbose_name='Тип')
+    user = models.ForeignKey(User,verbose_name='Пользователь',on_delete=models.CASCADE)
+    value = models.CharField(max_length=100, verbose_name='значение')
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Список контактов'
---
+++ tests/test_models.py
@@ -0,0 +1,298 @@
+import pytest
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from urllib.parse import quote
+from django.test import override_settings, modify_settings
+from rest_framework import serializers
+import backend.settings as settings
+from model_bakery import baker
+from app.models import Shop, Category,Product,\
+ProductInfo,Parameter,ProductParameter,Order,\
+OrderItem,Contact
+
+@pytest.fixture
+def shop_factory():
+    def factory(**kwargs):
+        return baker.make('Shop',**kwargs)
+    return factory
+
+@pytest.fixture
+def category_factory():
+    def factory(**kwargs):
+        return baker.make('Category',**kwargs)
+    return factory
+
+@pytest.fixture
+def product_factory():
+    def factory(**kwargs):
+        return baker.make('Product',**kwargs)
+    return factory
+
+@pytest.fixture
+def product_info_factory():
+    def factory(**kwargs):
+        return baker.make('ProductInfo',**kwargs)
+    return factory
+
+@pytest.fixture
+def parameter_factory():
+    def factory(**kwargs):
+        return baker.make('Parameter',**kwargs)
+    return factory
+
+@pytest.fixture
+def product_parameter_factory():
+    def factory(**kwargs):
+        return baker.make('ProductParameter',**kwargs)
+    return factory
+
+@pytest.fixture
+def order_factory():
+    def factory(**kwargs):
+        return baker.make('Order',**kwargs)
+    return factory
+
+@pytest.fixture
+def order_item_factory():
+    def factory(**kwargs):
+        return baker.make('OrderItem',**kwargs)
+    return factory
+
+@pytest.fixture
+def contact_item_factory():
+    def factory(**kwargs):
+        return baker.make('Contact',**kwargs)
+    return factory
+
+@pytest.mark.django_db
+class TestShop:
+
+    @pytest.fixture
+    def correct_test_data(self,shop_factory):
+        shops = []
+        for i in range(1,4):
+            shops.append(shop_factory(name = f'testname{i}',
+                            url = f'testurl{i}',
+                            filename = f'testfilename{i}'))
+        return shops
+
+    def test_create_shop(self,correct_test_data):
+        shop = correct_test_data[0]
+
+        assert isinstance(shop, Shop)
+        assert shop.id == 1
+        assert shop.name == 'testname1'
+        assert shop.url == 'testurl1'
+        assert shop.filename == 'testfilename1'
+
+    def test_update_shop(self,correct_test_data):
+        shop = correct_test_data[1]
+        update_data = {'name':'testname_update',
+                        'url':'testurl_update',
+                        'filename':'testfilename_update'}
+        
+        shop.name = update_data['name']
+        shop.url = update_data['url']
+        shop.filename = update_data['filename']
+        shop.save()
+        update_shop = Shop.objects.get(id = shop.id)
+
+        assert update_shop.name == update_data['name']
+        assert update_shop.url == update_data['url']
+        assert update_shop.filename == update_data['filename']
+
+    def test_shop_deletion(self, correct_test_data):
+        shop = correct_test_data[2]
+        shop.delete()
+        with pytest.raises(Shop.DoesNotExist):
+            Shop.objects.get(id=shop.id)
+
+@pytest.mark.django_db
+class TestCategory:
+
+    @pytest.fixture
+    def correct_test_data(self,category_factory,shop_factory):
+        categorys = []
+        for i in range(1,4):
+            shop = shop_factory(name=f'testname{i}',
+                            url=f'testurl{i}',
+                            filename=f'testfilename{i}')
+            category = category_factory(name=f'testname{i}',
+                                        shops=[shop])
+            categorys.append(category)
+        return categorys
+    
+    def test_create_category(self,correct_test_data):
+        category = correct_test_data[0]
+        
+        assert category.name == 'testname1'
+        assert category.shops.all()[0].name == 'testname1'
+
+    def test_update_category(self,correct_test_data):
+        category = correct_test_data[1]
+        update_shop = correct_test_data[0].shops.first()
+        update_name = 'update_name'
+
+        category.name = update_name
+        category.shops.set([update_shop])
+        category.save()
+        update_category = Category.objects.get(id = category.id)
+
+        assert update_category.name == update_name
+        assert update_category.shops.first() == update_shop
+
+    def test_category_deletion(self,correct_test_data):
+        category = correct_test_data[0]
+        category.delete()
+        with pytest.raises(Category.DoesNotExist):
+            Category.objects.get(id=category.id)
+
+@pytest.mark.django_db
+class TestProduct:
+
+    @pytest.fixture
+    def correct_test_data(self,product_factory,category_factory):
+        products = []
+        for i in range(1,4):
+            category = category_factory(name=f'cname{i}')
+            product = product_factory(name=f'pname{i}',category=category)
+            products.append(product)
+        return products
+
+    def test_create_product(self,correct_test_data):
+        pass
+
+    def test_update_product(self, correct_test_data):
+        pass
+
+    def test_product_deletion(self,correct_test_data):
+        pass
+
+@pytest.mark.django_db
+class TestProductInfo:
+
+    @pytest.fixture
+    def correct_test_data(self,product_info_factory,product_factory,shop_factory):
+        products_info = []
+        for i in range(1,4):
+            product = product_factory(name=f'pname{i}')
+            shop = shop_factory(name=f'sname{i}')
+            product_info = product_info_factory(product=product,
+                                                shop=shop,
+                                                name=f'piname{i}',
+                                                quantity=i,
+                                                price=i,
+                                                price_rrc='rub')
+            products_info.append(product_info)
+        return products_info
+
+
+
+    def test_create_product_info(self,correct_test_data):
+        pass
+
+    def test_update_product_info(self, correct_test_data):
+        pass
+
+    def test_product_info_deletion(self,correct_test_data):
+        pass
+
+@pytest.mark.django_db
+class TestParameter:
+
+    @pytest.fixture
+    def correct_test_data(self,parameter_factory):
+        parameters = []
+        for i in range(1,4):
+            parameter = parameter_factory(name=f'name{i}')
+            parameters.append(parameter)
+        return parameters
+
+    def test_create_parameter(self,correct_test_data):
+        pass
+
+    def test_update_parameter(self, correct_test_data):
+        pass
+
+    def test_parameter_deletion(self,correct_test_data):
+        pass
+
+@pytest.mark.django_db
+class TestProductParameter:
+
+    @pytest.fixture
+    def correct_test_data(self,product_parameter_factory):
+        #products_parameters = []
+        #for i in range(1,4):
+            
+
+        pass
+
+    def test_create_product_parameter(self,correct_test_data):
+        pass
+
+    def test_update_product_parameter(self, correct_test_data):
+        pass
+
+    def test_product_parameter_deletion(self,correct_test_data):
+        pass
+
+@pytest.mark.django_db
+class TestOrder:
+
+    @pytest.fixture
+    def correct_test_data(self):
+        pass
+
+    def test_create_order(self,correct_test_data):
+        pass
+
+    def test_update_order(self, correct_test_data):
+        pass
+
+    def test_order_deletion(self,correct_test_data):
+        pass
+
+@pytest.mark.django_db
+class TestOrderItem:
+
+    @pytest.fixture
+    def correct_test_data(self):
+        pass
+
+    def test_create_order_item(self,correct_test_data):
+        pass
+
+    def test_update_order_item(self, correct_test_data):
+        pass
+
+    def test_order_item_deletion(self,correct_test_data):
+        pass
+
+@pytest.mark.django_db
+class TestContact:
+
+    @pytest.fixture
+    def correct_test_data(self):
+        pass
+
+    def test_create_contact(self,correct_test_data):
+        pass
+
+    def test_update_contact(self, correct_test_data):
+        pass
+
+    def test_contact_deletion(self,correct_test_data):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
